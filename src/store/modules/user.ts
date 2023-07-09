dc1aa94f1762ac2b3b2be541dc4a65dfe3a7f228
wayne <xutenglongxutenglong@outlook.com>
2023-07-09 21:22:32 +0800

    ✨ feat: 封装请求响应拦截器

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: 徐腾龙
+ * @Date: 2023-07-07 23:29:05
+ * @LastEditors: 徐腾龙
+ * @LastEditTime: 2023-07-09 21:11:45
+ * @Description: 
+ * @FilePath: \v3_ts_shop\src\store\modules\user.ts
+ */
 import { defineStore } from "pinia";
 import type { profile } from "./types/type";
 // 创建用户小仓库
@@ -5,13 +13,13 @@ const useUserStore = defineStore("User", {
   // 小仓库存储数据地方
   state: () => {
     return {
-      userInfo: {} as profile
+      userInfo: {} as profile | {}
     };
   },
   getters: {},
   actions: {
-    setUser (info: profile) {
-        this.userInfo = info
+    setUser (info: profile | {}) {
+        this.userInfo = info || {}
     }
   }
 });
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,7 +2,7 @@
  * @Author: 徐腾龙
  * @Date: 2023-07-07 17:31:29
  * @LastEditors: 徐腾龙
- * @LastEditTime: 2023-07-08 00:18:19
+ * @LastEditTime: 2023-07-09 21:19:06
  * @Description: 
  * @FilePath: \v3_ts_shop\src\utils\request.ts
  */
@@ -18,17 +18,43 @@ const instance = axios.create({
     timeout: 5000
 })
 
+const userStore = useUserStore();
+
 instance.interceptors.request.use(config => {
     // 拦截业务逻辑
     // 进行请求配置的修改
     // 如果本地有token就在头部携带
     // 1. 获取用户信息对象
-    const userStore = useUserStore()
-    if (userStore.userInfo.token) {
+    if (userStore.userInfo?.token) {
         // 设置token
-        config.headers.Authorization = `Bearer${userStore.userInfo.token}`
+        config.headers.Authorization = `Bearer${userStore.userInfo?.token}`
     }
     return config
 }, err => {
     return Promise.reject(err)
 })
+
+// res => res.data 取出data数据，将来调用接口的时候直接拿到的就是后台的数据
+instance.interceptors.response.use(res => res.data, err => {
+    // 401状态码，进入该函数
+    if (err.response && err.response.status === 401) {
+        // 1. 清空无效用户信息
+        // 2. 跳转到登录页
+        // 3. 跳转需要传参（当前路由地址）给登录页码
+        userStore.setUser({})
+        // js模块中：router.currentRoute.value.fullPath 就是当前路由地址，router.currentRoute是ref响应式数据
+        const fullPath = encodeURIComponent(router.currentRoute.value.fullPath)
+        router.push('/login?redirectUrl=' + fullPath)
+    }
+    return Promise.reject(err)
+})
+
+// 请求工具函数
+export default (url: string, method: string, submitData: object) => {
+    // 负责发请求：请求地址，请求方式，提交的数据
+    return instance({
+        url,
+        method,
+        [method.toLowerCase() === 'get'? 'params': 'data']: submitData
+    })
+}
