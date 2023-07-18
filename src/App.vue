f7bd2847289ee0b6c2cbc01cc13b24232c95978d
Wayne1180 <xutenglongxutenglong@outlook.com>
2023-07-18 12:30:16 +0800

    🧪 test: -

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: 徐腾龙
  * @Date: 2023-07-07 20:24:25
  * @LastEditors: 徐腾龙
- * @LastEditTime: 2023-07-14 12:20:25
+ * @LastEditTime: 2023-07-14 12:27:16
  * @Description: 
  * @FilePath: \v3_ts_shop\src\App.vue
 -->
@@ -13,6 +13,15 @@
 </template>
 
 <script setup lang="ts">
+import numeral from 'numeral'
+let num = 3.1465926
+let numeralNum = numeral(num).format('0.00')
+console.log("🚀 ~ file: App.vue:19 ~ numeralNum:", numeralNum)
+let roundNum = Math.round(num * 100) / 100
+console.log("🚀 ~ file: App.vue:21 ~ roundNum:", roundNum)
+let fixedNum = num.toFixed(2)
+console.log("🚀 ~ file: App.vue:23 ~ fixedNum:", fixedNum)
+
 </script>
 
 <style lang="scss" scoped>
